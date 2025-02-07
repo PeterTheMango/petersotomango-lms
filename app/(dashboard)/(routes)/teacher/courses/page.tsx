fbc25e2391c32fb8d606fb9c64564f5b7f196355
PeterTheMango <pyeshuajs23@gmail.com>
2025-02-07 15:41:54 +0300

    Add file upload functionality, implement course management API, and enhance UI components

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const Teacher = () => {
-  return <div>Teacher</div>;
+  return (
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>Create a New Course</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default Teacher;
---
+++ app/api/courses/route.ts
@@ -0,0 +1,25 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  console.log(1);
+  try {
+    const { userId } = await auth();
+    const { title } = await req.json();
+
+    if (!userId) return new NextResponse("Unauthorized!", { status: 401 });
+
+    const course = await db.course.create({
+      data: {
+        userId,
+        title,
+      },
+    });
+
+    return NextResponse.json(course);
+  } catch (e) {
+    console.log(`[Courses]`, e);
+    return new NextResponse("Internal Error Occurred", { status: 500 });
+  }
+}
